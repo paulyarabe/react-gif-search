fbff4554b7ca78accbe0494ae0f78f4b1614b877
github username <github email address>
2017-11-07 21:00:05 -0500

    gif array read, api next

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import GifList from './components/GifList';
 import SearchBar from './components/SearchBar'
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      gifs: [
+        {
+          id: 1,
+          url: 'http://fakeimg.pl/300/'
+        },
+        {
+          id: 2,
+          url: 'http://fakeimg.pl/300/'
+        },
+        {
+          id: 3,
+          url: 'http://fakeimg.pl/300/'
+        }
+      ]
+    }
+  }
 
   handleTermChange(term) {
     console.log(term)
@@ -12,7 +33,8 @@ class App extends React.Component {
     return (
       <div>
         <SearchBar onTermChange={this.handleTermChange} />
-      </div>
+        <GifList gifs={this.state.gifs} />
+    </div>
     )
   }
 }
