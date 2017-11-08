52c42443e7df71464fc2ecdf96a5c2e4d534f7c1
github username <github email address>
2017-11-07 21:20:07 -0500

    giphy api call success

--- src/index.js
+++ src/index.js
@@ -1,32 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import GifList from './components/GifList';
-import SearchBar from './components/SearchBar'
+import SearchBar from './components/SearchBar';
+import request from 'superagent'
 
 class App extends React.Component {
   constructor() {
     super();
 
     this.state = {
-      gifs: [
-        {
-          id: 1,
-          url: 'http://fakeimg.pl/300/'
-        },
-        {
-          id: 2,
-          url: 'http://fakeimg.pl/300/'
-        },
-        {
-          id: 3,
-          url: 'http://fakeimg.pl/300/'
-        }
-      ]
+      gifs: []
     }
   }
 
-  handleTermChange(term) {
-    console.log(term)
+  handleTermChange = (term) => {
+    const url = `https://api.giphy.com/v1/gifs/search?q=${term.replace(/\s/g, '+')}&api_key=meAQD2mqIVAuFFa9DQRzMlMhExBUERPF`;
+
+    request.get(url, (err, res) => {
+      this.setState({ gifs: res.body.data});
+      console.log(res.body.data)
+    })
   }
 
   render() {
